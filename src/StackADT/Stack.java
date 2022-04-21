f4593212ea98b1d7a1d3e3d702abb860135bdb17
George Matta <jojomedhat2004@gmail.com>
2022-04-21 16:15:22 -0700

    Implemented stack class using rl

--- src/StackADT/Stack.java
+++ src/StackADT/Stack.java
@@ -4,34 +4,67 @@ import src.ListADT.*;
 
 public class Stack<T> implements StackInterface<T>{
 
-    @Override
-    public void push(T entry) {
-        // TODO Auto-generated method stub
+    private ResizeableList<T> stackList;
 
+    private boolean integrityOk = false;
+
+	private static final int DEFAULT_CAPACITY = 10;
+
+    public Stack(){
+      this(DEFAULT_CAPACITY);
+    }
+
+    public Stack(int initialCapacity){
+        stackList = new ResizeableList<T>(initialCapacity);
+        integrityOk = true;
+    }
+
+    /**
+     * Checks the integrity of the Stack to make sure the constructor was
+     * called.
+     *
+     * @throws SecurityException if the stack object was corrupted in some way or the
+     *                           constructor wasn't run properly.
+     */
+    private void checkIntegrity(){
+        if (!integrityOk) {
+            throw new SecurityException("ResizeableList object is corrupt or was not initialized properly.");
+        }
     }
 
     @Override
-    public T pop() {
+    public void push(T entry){
         // TODO Auto-generated method stub
-        return null;
+        checkIntegrity();
+        stackList.add(entry);
     }
 
     @Override
-    public T peek() {
+    public T pop(){
         // TODO Auto-generated method stub
-        return null;
+        checkIntegrity();
+        return stackList.remove();
     }
 
     @Override
-    public boolean isEmpty() {
+    public T peek(){
         // TODO Auto-generated method stub
-        return false;
+        checkIntegrity();
+        return stackList.get(stackList.getNumEntries());
     }
 
     @Override
-    public void clear() {
+    public boolean isEmpty(){
         // TODO Auto-generated method stub
+        checkIntegrity();
+        return stackList.isEmpty();
+    }
 
+    @Override
+    public void clear(){
+        // TODO Auto-generated method stub
+        checkIntegrity();
+        stackList.clear();
     }
 
 }
