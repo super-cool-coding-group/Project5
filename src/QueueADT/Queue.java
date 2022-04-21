5dd9cebf131a7b61c47164eda832acf3d5a97d15
George Matta <jojomedhat2004@gmail.com>
2022-04-21 15:51:59 -0700

    Implemented QueueInterface

--- src/QueueADT/Queue.java
+++ src/QueueADT/Queue.java
@@ -2,4 +2,34 @@ package src.QueueADT;
 
 public class Queue<T> implements QueueInterface<T>{
 
+    @Override
+    public void enqueue(T entry) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public T dequeue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T getFront() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
--- src/QueueADT/QueueInterface.java
+++ src/QueueADT/QueueInterface.java
@@ -1,5 +1,46 @@
 package src.QueueADT;
 
+/**
+ * An interface to implement methods related to Queues.
+
+ * @author George Matta
+ * @version 1.0
+ */
 public interface QueueInterface<T>{
+
+    /**
+     * Adds a new entry to the back of this queue.
+     *
+     * @param entry An object to be added.
+     */
+    public void enqueue(T entry);
+
+    /**
+     * Removes and returns the entry at the front of this queue.
+     *
+     * @return The object at the front of the queue.
+     * @throws EmptyQueueException if the queue is already empty.
+     */
+    public T dequeue();
+
+    /**
+     * Gets the entry at the front of this queue.
+     *
+     * @return The object at the front of the queue.
+     * @throws EmptyQueueException if the queue is empty.
+     */
+    public T getFront();
+
+    /**
+     * Returns whether or not this queue is empty.
+     *
+     * @return True if the queue is empty, or false otherwise.
+     */
+    public boolean isEmpty();
+
+    /**
+     * Removes all entries from this queue.
+     */
+    public void clear();
 
 }
