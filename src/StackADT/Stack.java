97d0360baabe396f2f3c3e9183a82d257e1a7e5a
George Matta <jojomedhat2004@gmail.com>
2022-04-21 15:47:33 -0700

    Implemented StackInterface

--- src/StackADT/Stack.java
+++ src/StackADT/Stack.java
@@ -2,4 +2,34 @@ package src.StackADT;
 
 public class Stack<T> implements StackInterface<T>{
 
+    @Override
+    public void push(T entry) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public T pop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
--- src/StackADT/StackInterface.java
+++ src/StackADT/StackInterface.java
@@ -1,5 +1,46 @@
 package src.StackADT;
 
+/**
+ * An interface to implement methods related to Stacks.
+
+ * @author George Matta
+ * @version 1.0
+ */
 public interface StackInterface<T>{
+
+    /**
+     * Adds a new entry to the top of this stack.
+     *
+     * @param entry An object to be added to the stack.
+     */
+    public void push(T entry);
+
+    /**
+     * Removes and returns this stack's top entry.
+     *
+     * @return The object at the top of the stack.
+     * @throws EmptyStackException if the stack is empty before the operation.
+     */
+    public T pop();
+
+    /**
+     * Gets the item at the top of the stack.
+     *
+     * @return The object at the top of the stack.
+     * @throws EmptyStackException if the stack is empty.
+     */
+    public T peek();
+
+    /**
+     * Returns whether or not this stack is empty.
+     *
+     * @return True if the stack is empty.
+     */
+    public boolean isEmpty();
+
+    /**
+     * Removes all items from this stack.
+     */
+    public void clear();
 
 }
