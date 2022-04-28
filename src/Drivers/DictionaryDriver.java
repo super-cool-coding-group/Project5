b6b041051ff0d457c8994d1ffc3c08979b3cb227
George Matta <jojomedhat2004@gmail.com>
2022-04-27 23:02:57 -0700

    added a dictionary driver

---
+++ src/Drivers/DictionaryDriver.java
@@ -0,0 +1,17 @@
+package src.Drivers;
+
+/**
+ * The DictionaryDriver class that tests basic functionality of the
+ * DictionaryDriver class.
+ *
+ * The class only has one method: a main method.
+ *
+ * @author George Matta
+ * @author Angelica Arteaga
+ * @version 1.0
+ */
+public class DictionaryDriver {
+    public static void main(String[] args){
+
+    }
+}
